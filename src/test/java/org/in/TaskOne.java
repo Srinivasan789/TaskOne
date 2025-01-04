993051c7d830c1240cb18bcf7fc7065cf680f51c
Srinivasan789 <srisivaconstruct@gmail.com>
2025-01-04 19:16:38 +0530

    new local added

---
+++ src/test/java/org/in/TaskOne.java
@@ -0,0 +1,17 @@
+package org.in;
+
+public class TaskOne {
+private void method1() {
+System.out.println("Kathikeyan");
+
+}
+private void method2() {
+System.out.println("Kanthan");
+}
+private void method3() {
+System.out.println("Kadamban");
+}
+private void method4() {
+System.out.println("Kathirvelan");
+}
+}
